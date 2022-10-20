070b10a19d781c789a7c4ec8c31c6659d10f45a0
kharinavi <kharinavi@mail.ru>
2022-10-20 22:42:02 +0400

    DI update

--- src/main/java/ru/kharina/study/music/TestMusic.java
+++ src/main/java/ru/kharina/study/music/TestMusic.java
@@ -7,9 +7,10 @@ public class TestMusic {
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
             "applicationContext.xml"
     );
-    Music music = context.getBean("musicBean", Music.class);
-    MusicPlayer musicPlayer = new MusicPlayer(music);
-    musicPlayer.playMusic();
-    context.close();
-}
+    //Music music = context.getBean("musicBean", Music.class);
+    //MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        context.close();
+    }
 }
